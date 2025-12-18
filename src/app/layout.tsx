c1bc2cd98a3d9e7ef9de3e4757a5e8c18c32323a
RicadGobul <wiliamricad0@gmail.com>
2025-12-18 20:10:11 +0700

    Add Facebook domain verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="bdy70w0q5pc4cuufmo44tyrx6m1jkm" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
